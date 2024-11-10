88677222e2e2be9789dc60ae6431b0611fc07638
Eder <edervb@gmail.com>
2024-11-10 00:05:04 -0300

    first commit

---
+++ test/pet.spec.js
@@ -0,0 +1,26 @@
+// Bibliotecas e frameworks
+const supertest = require('supertest')
+
+const petId = 906898801
+
+// Em JavaScript, classe é opcional, mas pode agrupar em uma Describe
+describe('API PetStore Swagger - Entidade Pet', () => {
+
+    // Atributos do grupo/describe
+    const request = supertest('https://petstore.swagger.io/v2') // BaseURL
+    
+    // Funções ou métodos: its
+    it('POST Pet', () => {
+        // Atributos, Campos, Características, Configurações
+        
+        // Funções de apoio (Opcional)
+
+        // Funções de Teste em Si
+
+    }) // Final do métodos POST
+
+
+}) // // termina a describe
+
+
+
